7712a97b9f044db1d9780b30e035c6fbfb25ea4e
Valerii Zhirnov <vazhirnov@yamoney.ru>
2019-04-26 17:05:49 +0300

    Fix publishing and git

--- src/main/java/ru/yandex/money/gradle/plugins/library/publishing/PublishingConfigurer.java
+++ src/main/java/ru/yandex/money/gradle/plugins/library/publishing/PublishingConfigurer.java
@@ -35,6 +35,8 @@ public class PublishingConfigurer {
      * Иницализация
      */
     public void init(Project project) {
+        project.getExtensions().getExtraProperties().set("groupIdSuffix", "");
+        project.getExtensions().getExtraProperties().set("artifactID", project.getName());
         project.getPluginManager().apply(MavenPublishPlugin.class);
         configureJavadoc(project);
         project.afterEvaluate(p -> {
@@ -63,7 +65,7 @@ public class PublishingConfigurer {
         publishingExtension.publications(publicationContainer -> {
             MavenPublication mavenJava = publicationContainer.maybeCreate("mavenJava", MavenPublication.class);
             mavenJava.setGroupId("ru.yandex.money" + groupIdSuffix(project));
-            mavenJava.setArtifactId(project.getName());
+            mavenJava.setArtifactId((String) project.property("artifactID"));
             mavenJava.from(getPublishingComponent(project));
             mavenJava.artifact(project.getTasks().getByName("sourceJar"), mavenArtifact -> mavenArtifact.setClassifier("sources"));
             mavenJava.artifact(project.getTasks().getByName("javadocJar"), mavenArtifact -> mavenArtifact.setClassifier("javadoc"));
@@ -93,7 +95,7 @@ public class PublishingConfigurer {
         Task storeVersion = project.getTasks().create("storeVersion");
         storeVersion.setDescription("Generates file, which contains information about build version");
         storeVersion.doLast(task -> {
-            String debVersion = String.format("%s:%s", project.getName(), project.getVersion());
+            String debVersion = String.format("%s:%s", project.property("artifactID"), project.getVersion());
             storeVersionToFile(project.getBuildDir().getAbsolutePath(), "version.txt", debVersion);
         });
     }
